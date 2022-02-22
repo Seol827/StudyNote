ece5a69be5bfb616208556049cf12fa2c40a8da4
hmhmchm <hyeonminchoi5@gmail.com>
2022-02-23 01:18:57 +0900

    feat: to-do list 연결 추가

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app.secret_key = 'seora'
 import config
 
 from pymongo import MongoClient
+
 client = MongoClient(config.Mongo_key)
 db = client.dbsparta
 
@@ -13,16 +14,74 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+@app.route('/todo/home')
+def todo_home():
+    return render_template('todolist.html')
 
-@app.route("/dbtest", methods=["GET"])
-def homework_get():
-    dblist = list(db.bucket.find({},{'_id':False}))
-    print(dblist);
-    return jsonify({'fans':dblist})
+@app.route('/loginresult', methods=["POST"])
+def login_result():
+    id_receive = request.form['userid']
+    password_receive = request.form['psw']
+
+    check_user_id = db.user.find_one({'ID': id_receive})
+
+    if check_user_id is None:
+        flash('회원 정보를 확인해 주세요.')
+        return redirect('/')
+    else:
+        if check_user_id.get("password") == password_receive:
+            user_name = check_user_id.get("name")
+            session['userid'] = user_name
+            return redirect("/")
+        else:
+            flash('회원 정보를 확인해 주세요.')
+            return redirect("/")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('userid', None)
+    return redirect('/')
+
+
+@app.route("/signup")
+def signup_form():
+    return render_template('signup.html')
+
+
+@app.route("/signupresult", methods=["POST"])
+def signup_result():
+    id_receive = request.form['userid']
+    password_receive = request.form['psw']
+    irum_receive = request.form['irum']
+
+    doc = {
+        'ID': id_receive,
+        'password': password_receive,
+        'name': irum_receive
+    }
+
+    db.user.insert_one(doc)
+    return redirect("/")
+
+
+@app.route("/idcheck", methods=["POST"])
+def id_check():
+    id_receive = request.form['ID']
+
+    check_user_id = db.user.find_one({'ID': id_receive})
+
+    if check_user_id is not None:
+        if check_user_id['ID']:
+            return 'fail'
+            print('fail');
+    return 'success'
+
+### to do list ###
 
 @app.route("/todo", methods=["POST"])
 def todo_post():
-    all_todos = list(db.todo.find({}, {'_id': False}))
+    all_todos = list(db.todos.find({}, {'_id': False}))
     count = len(all_todos) + 1
     todo_receive = request.form['todo_give']
 
@@ -32,21 +91,21 @@ def todo_post():
         'done' : 0
     }
 
-    db.todo.insert_one(doc)
+    db.todos.insert_one(doc)
     return jsonify({'msg': '저장완료!'})
-
 
 @app.route("/todo/done", methods=["POST"])
 def todo_done():
 
     num_receive = request.form['num_give']
-    db.todo.update_one({'num' : int(num_receive)},{'$set' : {'done': 1} })
+    db.todos.update_one({'num' : int(num_receive)},{'$set' : {'done': 1} })
     return jsonify({'msg': '완료!'})
 
 
 @app.route("/todo", methods=["GET"])
 def todo_get():
-    todo_list = list(db.todo.find({}, {'_id': False}))
+    id = list(db.user.find({}, {'_id': False, 'password': False, 'name': False}))
+    todo_list = list(db.todos.find({}, {'_id': False}))
 
     return jsonify({'todos': todo_list})
 
@@ -54,8 +113,15 @@ def todo_get():
 def todo_delete():
 
     num_receive = request.form['num_give']
-    db.todo.delete_one({'num': int(num_receive)})
+    db.todos.delete_one({'num': int(num_receive)})
 
     return jsonify({'msg':'삭제완료'})
+
+# @app.route("/todo/id", methods=["GET"])
+# def todo_id():
+#     id = list(db.user.find({}, {'_id': False, 'password': False, 'name' : False}))
+#
+#     return jsonify({'id': id})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8000, debug=True)
